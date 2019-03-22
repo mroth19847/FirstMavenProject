ee9a708a0a92de26bcfb69d1c341b7fe104f53a2
Maximilian ROTHWANGL <rotmac16@htlkaindorf.at>
2019-03-22 11:59:32 +0100

    changed the AnlagenTest file

--- src/test/java/htl/rothwangl/firstmavenproject/AnlageTest.java
+++ src/test/java/htl/rothwangl/firstmavenproject/AnlageTest.java
@@ -6,6 +6,7 @@
 package htl.rothwangl.firstmavenproject;
 
 import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
 
 /**
  *
@@ -16,17 +17,20 @@ public class AnlageTest {
     public AnlageTest() {
     }
 
+    @Test
     public void testJahresAfa() {
         Anlage a = new Anlage(2000.0, 10);
         assertEquals(200.0, a.jahresAfa(), 0.01);
     }
     
+    @Test
     public void testBuchwert1(){
         Anlage a = new Anlage(2000.0, 10);
         a.updateBuchwert(5);
         assertEquals(1000.0, a.getBuchwert(), 0.01);
     }
     
+    @Test
     public void testBuchwert2(){
         Anlage a = new Anlage(2000.0, 10);
         a.updateBuchwert(12);
